aa69778aa7435efd0178ae01b2441eb3fbb0659d
Josh Hootman <josh.hootman809@gmail.com>
2020-08-31 19:46:39 -0500

    method to handle form submission. Creates html elements and creates object

--- script.js
+++ script.js
@@ -12,32 +12,20 @@ function Book(title, author, pages, read){
 // Book.prototype.info = function() {
 //     return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
 // }
+Book.prototype.readStatus = function() {
+    console.log(this.read)
+}
 
-function addBookToLibrary(title, author, page, readStatus){
-    const book = new Book(title, author, page, readStatus);
-    myLibrary.push(book);
+//okay so add a button for read status else where
+//then use this prototype to toggle a books read status (probs will just flip the boolean value)
+Book.prototype.updateReadStatus = function() {
+    myLibrary.forEach(book => {
+        book.readStatus()
+    })
 }
 
 
-
-function deleteButton(){}
-
-function render(){
-    const tableBody = document.querySelector("#books");
-    myLibrary.forEach((book, index) =>  {
-        console.log(index)
-        const tableRow = document.createElement('tr');
-        tableRow.setAttribute("data-id", index);
-        tableBody.append(tableRow);
-
-
-        const values = Object.values(book);
-        values.forEach(value => {
-            const tableCell = document.createElement('td');
-            tableCell.textContent = value;
-            tableRow.append(tableCell)
-
-        })
+function addDeleteBtn(tableRow){
         //create new cell for button
         const tableCell = document.createElement('td');
         tableRow.append(tableCell);
@@ -53,9 +41,56 @@ function render(){
             row.remove();
             delete myLibrary[rowID];
         });
+}
+
+function addBookToLibrary(title, author, page, readStatus){
+    const book = new Book(title, author, page, readStatus);
+    myLibrary.push(book);
+
+    let tableRow = renderRow(book, (myLibrary.length - 1));
+    addDeleteBtn(tableRow);
+}
+
+
+//okay so this could either be used to update the data id's or to add the odd number class.
+// I'm thinking that the addBookToLibrary function would still be best for adding html.
+function updateTable(){
+    // const tableBody = document.querySelector("#books");
+    const tableRows = document.querySelectorAll(".table-row");
+    tableRows.forEach(row => {
+        console.log(row.getAttribute('data-id'))
+    });
+};
+
+function renderRow(book, index){
+    const tableBody = document.querySelector("#books");
+    const tableRow = document.createElement('tr');
+    tableRow.setAttribute("data-id", index);
+    tableRow.classList.add("table-row");
+    tableBody.append(tableRow);
+
+    const values = Object.values(book);
+    values.forEach(value => {
+        const tableCell = document.createElement('td');
+        tableCell.textContent = value;
+        tableRow.append(tableCell)
 
     });
+    return tableRow
 }
+
+
+function render(){
+    myLibrary.forEach((book, index) => {
+        let tableRow = renderRow(book, index);
+        addDeleteBtn(tableRow);
+    });
+           
+}
+
+
+    
+
 
 const addBookBtn = document.querySelector('.open-button');
 addBookBtn.addEventListener('click', () => {
@@ -83,7 +118,15 @@ submitBtn.addEventListener('click', () => {
 
 });
 
+// To test add book
+// addBookToLibrary("The Hobbit", "J.R.R Tolkein", 675, false);
+// addBookToLibrary("The Return of the King", "Tolkein", 876, false);
 
+// To test Render
+const book1 = new Book("The Hobbit", "J.R.R Tolkein", 675, false);
+const book2 = new Book("The Return of the King", "Tolkein", 876, false);
+myLibrary.push(book1);
+myLibrary.push(book2);
 render();
 
 
@@ -93,10 +136,9 @@ render();
 // also need to puzzle out the Read (status) column.  Need it to change on click.  So will need an event listener that changes the
 // boolean value on click.  and then changes the element in the document.
 
-// may want to create button with a function to clean it up some. (and add the event listener) <- only worry is if it fucks up everything.
 // can use the index to add a class to each odd numbered tablerow/cell. this class will change the background color. 
 
-//so now the elements get deleted
+
 
 // to do the alternating table colors, use something other then delete so that indexs get reset.  then write a function that
 // will update the classes to set the background colors.
@@ -104,18 +146,4 @@ render();
 //lost the og styling inspiration but i found another https://georgius17.github.io/library/
 
 // newest issue: I will need to fix the render method so that it updates the html with the elements not already on the page.
-// maybe I can grab the tablebody element and loop trhough each tr data-id, if the id is there then don't update the book?
-// could also update the odd and even classes this way as well
-
-
-
-
-
-
-// function openForm() {
-//     document.getElementById("myForm").style.display = "block";
-// }
-
-// function closeForm() {
-//     document.getElementById("myForm").style.display = "none";
-// }
+//^ FIXED
